c7b45eee74d748d90baab8779362cb3bf1eab15b
Fahad ali siddiquie <165949614+fahadalisidd@users.noreply.github.com>
2025-09-19 00:27:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// Replace "health-chatbot" with your repo name
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/ai-health-chatbot/', 
+})
